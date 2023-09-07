2b100b57a1abf91dec2d6a3dca935f3ae0e4a67f
aligomaa56 <3looosh56565656@gmail.com>
2023-09-07 08:47:30 -0700

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unisdt.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program print a qoute using write function
+ *    ssize_t write (int fd,const void *buf ,size_t count);
+ *
+ * Return: Alawys 1 (not Success)
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
