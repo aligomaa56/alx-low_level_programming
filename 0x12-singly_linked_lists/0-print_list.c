9f5b7e4d08559735430ef38673725e301613d5f2
aligomaa56 <3looosh56565656@gmail.com>
2023-10-20 02:44:36 +0300

    fix9

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,14 @@
  */
 size_t print_list(const list_t *h)
 {
-	int s = 0;
+	size_t s = 0;
 
 	while (h)
 	{
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-		}
 		h = h->next;
 		s++;
 	}
