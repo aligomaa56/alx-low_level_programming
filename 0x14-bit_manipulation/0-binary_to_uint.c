296cc41b726a41cf2eabc72e6e8e59891f941cc5
aligomaa56 <3looosh56565656@gmail.com>
2023-10-26 23:22:05 +0300

    modified2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,20 +7,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
+	int i;
+	unsigned int num;
 
+	num = 0;
 	if (!b)
 		return (0);
-
-	if (*b != '1' && *b != '0')
-		return (0);
-
-	while (*b != '\0')
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		num *= 2;
-		num += (*b - '0');
-		b++;
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 	}
-
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+	}
 	return (num);
 }
